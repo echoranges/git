44cd22fe16fab77cf086fda2e20551e82a670a31
null <362270142@qq.com>
2015-03-17 16:17:32 +0800

    the first game

---
+++ wordgame_1.py
@@ -0,0 +1,9 @@
+print('--------------我爱鱼C工作室---------------')
+temp = input('不妨猜一下小甲鱼心里现在想的是那个数字：')
+guess = int(temp)
+if guess == 8:
+    print('卧槽，猜中了\n')
+    print('牛逼')
+else:
+    print('猜错')
+print('游戏结束')
